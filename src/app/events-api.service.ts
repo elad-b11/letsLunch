16e846ff2f0b05aaec3834453e44469882e3f2a9
eb <elad705@gmail.com>
2019-08-07 22:21:17 +0300

    Full event api

--- src/app/events-api.service.ts
+++ src/app/events-api.service.ts
@@ -7,18 +7,47 @@ import { HttpClient } from '@angular/common/http';
 export class EventsApiService {
 
   constructor(private httpClient: HttpClient) { 
+  
   }
+  baseUrl = "http://129.213.110.238:3000/events";
 
   getAllEvents():Promise<object[]> {
       return new Promise((resolve, reject) => {
-        this.httpClient.get("http://129.213.110.238:3000/events",
-                            {headers:
-                              {
-                                "Access-Control-Allow-Origin": "*"
-                              }
-                            }).subscribe((data) => {
+        this.httpClient.get(this.baseUrl).subscribe((data) => {
           resolve(data['results']);
         });
       }); 
   }
+
+  createEvent(restaurantName, eventDate, attendees, isJoinable, canOrderFrom, description):Promise<string> {
+    let eventObj = {
+      restaurantName,
+      eventDate: new Date(eventDate),
+      attendees,
+      isJoinable,
+      canOrderFrom,
+      description
+    }
+    return new Promise((resolve, reject) => {
+      this.httpClient.post(this.baseUrl, eventObj).subscribe((resultId) => {
+            resolve(resultId.toString());
+      });
+    });
+  }
+
+  deleteEvent(eventId):Promise<string> {
+    return new Promise((resolve,reject) => {
+      this.httpClient.delete(`${this.baseUrl}/${eventId}`).subscribe((deletedId) => {
+        resolve(deletedId.toString());
+      })
+    });
+  }
+
+  getEventById(eventId):Promise<object> {
+    return new Promise((resolve,reject) => {
+      this.httpClient.get(`${this.baseUrl}/${eventId}`).subscribe((event) => {
+        resolve(event);
+      })
+    });
+  } 
 }
